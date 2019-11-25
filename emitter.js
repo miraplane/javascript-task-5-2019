74888a34238207a398d56a3a740458a5cc1215d2
Irina Mochalova <IrinaMochalova@gmail.com>
2019-11-25 21:38:34 +0500

    Добавлен первый вариант решения

--- emitter.js
+++ emitter.js
@@ -12,32 +12,85 @@ const isStar = true;
  */
 function getEmitter() {
     return {
+        students: [],
+        events: [],
+        severalTimes: [],
+
+        checkHandler: function (handler) {
+            if (handler.hasOwnProperty('timer')) {
+                if (handler.timer === 0) {
+                    return false;
+                }
+                handler.timer -= 1;
+            }
+            if (handler.hasOwnProperty('every')) {
+                if (handler.count !== handler.every) {
+                    handler.count += 1;
+
+                    return false;
+                }
+                handler.count = 1;
+            }
+
+            return true;
+        },
+
+        callHandler: function (part) {
+            for (let student of this.students) {
+                if (student.hasOwnProperty(part) && this.checkHandler(student[part])) {
+                    student[part]();
+                }
+            }
+        },
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (this.students.indexOf(context) === -1) {
+                this.students.push(context);
+            }
+            if (this.events.indexOf(event) === -1) {
+                this.events.push(event);
+            }
+            context[event] = handler;
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (let e of this.events) {
+                if (e.indexOf(event) !== -1 && context.hasOwnProperty(e)) {
+                    delete context[e];
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            let handler = event.split('.');
+            for (let i = handler.length; i > 0; i--) {
+                let part = handler.slice(0, i).join('.');
+                this.callHandler(part);
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +100,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            this.on(event, context, handler);
+            if (times > 0) {
+                context[event].timer = times;
+            }
+
+            return this;
         },
 
         /**
@@ -59,9 +118,16 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            this.on(event, context, handler);
+            if (frequency > 0) {
+                context[event].count = frequency;
+                context[event].every = frequency;
+            }
+
+            return this;
         }
     };
 }
